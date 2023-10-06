5c9dbe4ceba6960ea9eac1429290cf2ef86d1749
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-10-05 18:56:38 -0700

    My first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n", char);
+	printf("Size of an int: %d bytes(s)\n", int);
+	printf("Size of a long int: %li byte(s)\n", long int);
+	printf("Size of long long int: %lld byte(s)\n",long long int);
+	printf("Size of a float: %f byte(s)\n", float);
+	return 0;
+}
