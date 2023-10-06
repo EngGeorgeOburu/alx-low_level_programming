62de1de7e513171f2b7fd52d67e0183fcad42ce4
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-10-05 20:05:05 -0700

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-/***
+/**
  * main - Entry point of the program
+ *
  * Description: A simple c program that prints a sentence with a comma
+ *
  * Return: Always 0 (success)
  */
 int main(void)
