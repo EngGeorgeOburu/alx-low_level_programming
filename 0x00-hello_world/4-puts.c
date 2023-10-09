f98837e86550312f6243a7f881a9d37d517ebe83
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-10-09 02:57:57 -0700

    My commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/**
- * main-Entry point of the program
- * 
- * Return: Always 0 (Success)
- */
+/* main-Entry point of the program */
+ 
+/* Return: Always 0 (success) */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
