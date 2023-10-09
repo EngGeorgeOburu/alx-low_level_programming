a9db701798807d4f211735718931fd9c734d4977
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-10-09 00:53:36 -0700

    My commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* Main - Entry point of the program */
+
+/* Description: A program using puts to print out */
+
+/* Return: Always 0 (success) */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a project that involves c_programming, a low level language
