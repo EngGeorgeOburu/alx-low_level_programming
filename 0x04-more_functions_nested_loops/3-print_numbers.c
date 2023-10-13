8e813372b77661165358aaade93e4cfa91c24afc
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-10-13 03:39:12 -0700

    Functions,nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
- * print_numbers:prints numbers from 0 to 9
- * Return:Always 0 (success)
+ * print_numbers - This function prints numbers from 0 to 9.
+ *
+ * Return:Always 0 (success).
  */
 void print_numbers(void)
 {
