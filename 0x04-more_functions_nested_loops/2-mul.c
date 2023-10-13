b5673d08c8c08004f79c44429c73ce476d43a984
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-10-13 03:31:31 -0700

    Functions,nested loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * main - Entry point
- * mul:multiplies two intergers
- * Return:Always 0 (success)
+ * mul - This function multiples two numbers.
+ *
+ * @a: The first number to be multiplied.
+ *
+ * @b: The second number to be multiplied.
+ *
+ * Return:Always 0 (success).
  */
-int mul(int a,int b)
+int mul(int a, int b)
 {
 	int total;
 
 	total = a * b;
-	return total;
+	return (total);
 }
