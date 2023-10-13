c2df64489ba29ee0a14fe08740a9cb2232e47ab4
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-10-13 16:28:46 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Check for a positive or negative number.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	positive_or_negative(c);
+	return (0);
+}
