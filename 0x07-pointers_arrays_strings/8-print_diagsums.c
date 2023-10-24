0e1010cc564e3e740ca8f6217467c1373d526bd5
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-10-23 23:20:26 -0700

    strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdioh.h>
+#include <stdio.h>
 /**
  * print_diagsums - Prints sum of two diagnols.
  * @size: The size of the array.
  * @a: The matrix used.
  */
-void print_diagsums(int *a,int size)
+void print_diagsums(int *a, int size)
 {
 	int total_minor = 0;
 	int total_major = 0;
@@ -14,6 +14,6 @@ void print_diagsums(int *a,int size)
 	for (i = 0; i < size; i++)
 	{
 		total_minor += a[i * size + i];
-		total_major += a[(size -1 -i) * size + i];
+		total_major += a[(size - 1 - i) * size + i];
 	}
-
+}
