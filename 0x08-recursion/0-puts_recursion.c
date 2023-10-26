4c8d5fb03b742400816230fef332bc001ebcf2f1
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-10-26 23:24:55 +0300

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	int num = 1;
-
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
-	_puts_recursion(s + num);
+	_puts_recursion(s + 1);
 }
 
