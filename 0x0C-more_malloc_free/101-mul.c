c743026ba5b72d3bac5fdfe30ede0974dedf8c7a
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-11-03 03:09:19 +0300

    advanced malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/****/
+/**
+ * main - Entry point of the program
+ * **/
 int main(int argc, char * argv[])
 {
 	int first_num = atoi(argv[1]);
