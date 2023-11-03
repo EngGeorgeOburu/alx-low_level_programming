7a34317839eeda72dc7307f33329069b82719961
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-11-03 14:24:49 +0300

    advanced malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	p = malloc(new_size);
 	if (new_size > old_size)
 	{
-		return (malloc (new_size));
+		return (malloc(new_size));
 	}
 	if (new_size == old_size)
 	{
