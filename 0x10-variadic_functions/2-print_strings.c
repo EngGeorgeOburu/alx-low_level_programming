95e30ebeffab6396fcb59be93f466bc1cbc0f465
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-11-10 02:44:07 +0300

    fixing betty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,8 +13,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	size_t i;
 	const char *str;
-	va_start(args, n);
 
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, const char *);
