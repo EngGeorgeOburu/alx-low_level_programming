df6bbbf4d20cfd36ae8ccc2042c613c13281b9a3
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-11-10 02:20:09 +0300

    fixing betty and output

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,21 +4,24 @@
 /**
  * print_numbers - function printing numbers
  * @n: Number of intergers
+ * @separator: string to be printed
  * Return: Nothing
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	size_t i;
+
 	va_start(args, n);
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
+		
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
 	}
 	va_end(args);
 	printf("\n");
