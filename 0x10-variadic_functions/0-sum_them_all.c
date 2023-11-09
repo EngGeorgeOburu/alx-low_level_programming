3ffd33c2059e8b0c703b8c3dda4450c2158f93e2
EngGeorgeOburu <oburugeorge94@gmail.com>
2023-11-10 01:51:14 +0300

    fixing betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,19 +4,21 @@
 /**
  * sum_them_all - return the sum of all its parameters
  * @n: The number parameter
- * Return: Sum**/
+ * Return: Sum
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	size_t i = 0;
 	int sum = 0;
 	va_list args;
+
 	va_start(args, n);
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	while (i < n )
+	while (i < n)
 	{
 		sum += va_arg(args, int);
 		i++;
