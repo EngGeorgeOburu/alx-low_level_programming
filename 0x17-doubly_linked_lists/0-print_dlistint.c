cd92d1555607708bd928473c4cb2f09d92692afd
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-01-11 15:17:59 +0300

    doubly linked

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+print_dlistint - Prints all elements in the dlistint_t list.
+@h: Pointer to the head of the list
+Return: Sum of nodes
+*/
+size_t print_dlistint(const dlistint_t *h);
+{
+	size_t node_counter = 0;
+
+	while (h)
+	{
+		node_counter++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (node_counter);
+}
+
+
