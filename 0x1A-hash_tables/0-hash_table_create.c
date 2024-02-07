7c9d0cdb02bd705433cc82f487e783b6734c5f4f
EngGeorgeOburu <oburugeorge94@gmail.com>
2024-02-07 12:39:48 +0300

    ficing betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,22 +6,22 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-   hash_table_t * ht = NULL; /* Pointer to the hash table */
+   hash_table_t *ht = NULL;/* Pointer to the hash table */
    unsigned long int i;
-    
-   ht = malloc(sizeof(hash_table_t));
-   if (ht == NULL)
-       return (NULL);
 
-   ht->array = malloc(sizeof(hash_table_t *) * size);
+   ht = malloc(sizeof(hash_table_t));/* Allocate memory for hash tables*/
+   if (ht == NULL)
+       return (NULL);/*Return NULL if malloc fails*/
+
+   ht->array = malloc(sizeof(hash_table_t *) * size);/*Alocate memeory for the array*/
    if (ht->array == NULL)
    {
-        free(ht);
-        return (NULL);
+        free(ht);/*free previosly allocated memory*/
+        return (NULL);/*Return NULL if malloc fails*/
     }
     for (i = 0; i < size; i++)
-        ht->array[i] = NULL;
+        ht->array[i] = NULL;/*Initializes each element of the array to NULL*/
 
-    ht->size = size;
-    return (ht);
+    ht->size = size;/*assign the size of the array*/
+    return (ht);/*Return pointer to the hash table*/
 }
--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -14,4 +14,3 @@ unsigned long int hash_djb2(const unsigned char *str)
     }
     return (hash);
 }
-
